5ea72ad2d9e030ae048ac94fdd20f70445ba396b
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-12 22:11:46 +0700

    refactor: improve code organization

--- src/helper.js
+++ src/helper.js
@@ -8,3 +8,4 @@
 // update 10 at Jum 12 Jun 2026 20:31:13 WIB
 // update 14 at Jum 12 Jun 2026 21:29:39 WIB
 // update 16 at Jum 12 Jun 2026 21:56:43 WIB
+// update 17 at Jum 12 Jun 2026 22:11:46 WIB
