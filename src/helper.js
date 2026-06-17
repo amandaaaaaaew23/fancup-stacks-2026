da5e215ee7da90bbc754b620fe123b6037236dfe
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-17 15:57:05 +0700

    chore: align project assets

--- src/helper.js
+++ src/helper.js
@@ -40,3 +40,4 @@
 // update 21 at Rab 17 Jun 2026 13:27:21 WIB
 // update 29 at Rab 17 Jun 2026 15:28:42 WIB
 // update 30 at Rab 17 Jun 2026 15:41:19 WIB
+// update 31 at Rab 17 Jun 2026 15:57:05 WIB
