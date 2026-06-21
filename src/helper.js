6e41de3ea3d1e4184f952cf736d46a39a4761b08
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-22 00:19:10 +0700

    refactor: tidy source structure

--- src/helper.js
+++ src/helper.js
@@ -85,3 +85,4 @@
 // update 135 at Min 21 Jun 2026 21:50:58 WIB
 // update 143 at Min 21 Jun 2026 23:52:26 WIB
 // update 144 at Sen 22 Jun 2026 00:05:06 WIB
+// update 145 at Sen 22 Jun 2026 00:19:10 WIB
